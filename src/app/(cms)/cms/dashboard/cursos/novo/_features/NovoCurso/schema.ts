4cf5f2ca4c80083e454d392882b15f0c3a00d579
rayrazer <cauaarrudaferreira@gmail.com>
2026-05-08 18:58:52 -0300

    schema price_cents string

--- src/app/(cms)/cms/dashboard/cursos/novo/_features/NovoCurso/schema.ts
+++ src/app/(cms)/cms/dashboard/cursos/novo/_features/NovoCurso/schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const cursoSchema = z.object({
-  title: z.string().min(3, 'Título obrigatório'),
-  description: z.string().optional(),
+  title: z.string().min(3, "Título deve ter no mínimo 3 caracteres"),
+  description: z.string().min(10, "Descrição deve ter no mínimo 10 caracteres"),
   price_cents: z.string().optional(),
   is_published: z.boolean(),
 });
